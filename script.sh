c03827ea66bd5ba4aefd05bdeb2b870f305895a1
anoop <abc@gmail.com>
2024-06-24 10:31:02 +0530

    test1

--- script.sh
+++ script.sh
@@ -4,31 +4,23 @@ set -e
 
 cd /home/anoop/dex
 
-# Check if the container is running and stop it if it is
+# Ensure the script has permissions to modify the git config
+chmod -R u+w .git
+
+# Stop the existing container if it is running
 if [ "$(docker ps -q -f name=nextapp1)" ]; then
     echo "Stopping running container nextapp1..."
     docker stop nextapp1
-else
-    echo "No running container named nextapp1 to stop."
 fi
 
-# Set the upstream branch if not already set
-git branch --set-upstream-to=origin/main main || true
-
-# Pull the latest changes from the git repository
+# Pull the latest code from the Git repository
 git pull origin main
 
 # Build the Docker image
 docker build -t nextapp1 .
 
-# Start the stopped container if it exists, otherwise run a new container
-if [ "$(docker ps -aq -f name=nextapp1)" ]; then
-    echo "Removing existing container nextapp1..."
-    docker rm  nextapp1
-else
-    echo "Running new container nextapp1..."
-    docker run -d -t -p 3000:3000 -v abc --name nextapp1 nextapp1
-fi
+# Run a new container with the same name
+docker run -d -t -p 3000:3000 -v abc --name nextapp1 nextapp1
 
 echo "[$(date)] Deployment script completed."
 
